792c8de57a011bee33e3854c89c346aebec83096
Andrew Mendez <andrewjeffmendez30@gmail.com>
2024-04-20 18:29:23 +0800

    feat: add timer per question

--- src/components/Quiz/Game.tsx
+++ src/components/Quiz/Game.tsx
@@ -1,14 +1,47 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { useQuiz } from "../../store";
 import { shuffleArray } from "../../utils/shuffleArray";
+import { replaceEntities } from "../../utils/replaceEntities";
+import StatusHeader from "./StatusHeader";
+
+const NUM_OF_QUESTIONS = 10;
 
 export default function Game() {
   const [itemIndex, setItemIndex] = useState(0);
   const [didAnswer, setDidAnswer] = useState(false);
   const quizItems = useQuiz((state) => state.quizItems);
   const type = useQuiz((state) => state.type);
-  const correctAns = quizItems[itemIndex].correct_answer;
-  const incorrectAns = quizItems[itemIndex].incorrect_answers;
+  const question = replaceEntities(quizItems[itemIndex].question);
+  const correctAns = replaceEntities(quizItems[itemIndex].correct_answer);
+  const incorrectAns = quizItems[itemIndex].incorrect_answers.map((item) =>
+    replaceEntities(item),
+  );
+
+  const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(15);
+
+  let timer: number;
+
+  useEffect(() => {
+    if (!didAnswer && timeLeft > 0) {
+      timer = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    }
+
+    if (didAnswer) {
+      clearInterval(timer);
+    }
+
+    if (timeLeft === 0) {
+      setDidAnswer(true);
+      revealAnswer();
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
 
   const choices = useMemo(() => {
     return type === "multiple"
@@ -16,71 +49,91 @@ export default function Game() {
       : ["True", "False"];
   }, [itemIndex]);
 
-  // const choices =
-  //   type === "multiple" ? memoizedShuffleArray : ["True", "False"];
+  const revealAnswer = () => {
+    const btns = document.querySelectorAll(".btn");
+    const searchText = correctAns;
+    let found;
+
+    for (var i = 0; i < btns.length; i++) {
+      if (btns[i].textContent == searchText) {
+        found = btns[i];
+        break;
+      }
+    }
+
+    found?.classList.remove("bg-slate-200");
+    found?.classList.add("border-green-500");
+  };
 
   const handleAnswer = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     choice: string,
   ) => {
     if (choice === correctAns) {
+      (event.target as Element).classList.remove("bg-slate-200");
       (event.target as Element).classList.add("bg-green-500");
+      (event.target as Element).classList.add("border-green-500");
+
+      setScore(score + 200);
     } else {
+      (event.target as Element).classList.remove("bg-slate-200");
       (event.target as Element).classList.add("bg-red-500");
+      (event.target as Element).classList.add("border-red-500");
 
-      const aTags = document.querySelectorAll(".btn");
-      const searchText = correctAns;
-      let found;
-
-      for (var i = 0; i < aTags.length; i++) {
-        if (aTags[i].textContent == searchText) {
-          found = aTags[i];
-          break;
-        }
-      }
-
-      console.log(typeof found);
-      found?.classList.add("bg-green-500");
+      revealAnswer();
     }
     setDidAnswer(true);
   };
 
   const handleNext = () => {
+    if (itemIndex === NUM_OF_QUESTIONS - 1) return;
+
     setItemIndex(itemIndex + 1);
     setDidAnswer(false);
-    const btns = document.querySelectorAll(".btn");
+    setTimeLeft(15);
 
+    const btns = document.querySelectorAll(".btn");
     btns.forEach((btn) => {
       btn.classList.remove("bg-green-500");
+      btn.classList.remove("border-green-500");
       btn.classList.remove("bg-red-500");
+      btn.classList.remove("border-red-500");
+      btn.classList.add("bg-slate-200");
     });
   };
 
   return (
-    <div>
+    <>
+      <StatusHeader
+        questionNumber={itemIndex + 1}
+        timeLeft={timeLeft}
+        score={score}
+      />
+
       <div className="flex min-h-28 items-center justify-center">
-        <p
-          className="text-center text-xl font-medium"
-          dangerouslySetInnerHTML={{ __html: quizItems[itemIndex].question }}
-        />
+        <p className="text-center text-xl font-medium">{question}</p>
       </div>
 
-      <div className="mb-4 grid gap-2">
+      <div className="mb-4 grid gap-4">
         {choices.map((choice, index) => (
           <button
-            className="btn rounded-md border border-slate-500 px-4 py-2 transition-colors"
+            className="btn rounded-md border-2 bg-slate-200 px-4 py-2 shadow-md transition-colors"
             key={index}
-            dangerouslySetInnerHTML={{ __html: choice }}
             onClick={(event) => handleAnswer(event, choice)}
-          />
+            disabled={didAnswer}
+          >
+            {choice}
+          </button>
         ))}
       </div>
+
+      <p dangerouslySetInnerHTML={{ __html: "&lrm;" }} />
 
       {didAnswer ? (
         <button className="bg-green-500 px-4 py-2" onClick={handleNext}>
           next
         </button>
       ) : null}
-    </div>
+    </>
   );
 }
--- src/components/Quiz/StatusHeader.tsx
+++ src/components/Quiz/StatusHeader.tsx
@@ -1,3 +1,37 @@
-export default function StatusHeader() {
-  return <div>StatusHeader</div>;
+export default function StatusHeader({
+  questionNumber,
+  timeLeft,
+  score,
+}: {
+  questionNumber: number;
+  timeLeft: number;
+  score: number;
+}) {
+  return (
+    <header className="bg-slate-400 p-2">
+      <div className="grid grid-cols-3">
+        <div className="-mr-[1px] flex flex-col items-center border border-slate-600">
+          <h2>Question</h2>
+          <p>{questionNumber}/10</p>
+        </div>
+        <div className="-mr-[1px] flex flex-col items-center border border-slate-600">
+          <h2>Time</h2>
+          <Timer timeLeft={timeLeft} />
+        </div>
+        <div className="flex flex-col items-center border border-slate-600">
+          <h2>Score</h2>
+          <p>{score}</p>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Timer({ timeLeft }: { timeLeft: number }) {
+  const formatTime = (time: number) => {
+    const seconds = time % 60;
+    return `:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  return <p>{formatTime(timeLeft)}</p>;
 }
