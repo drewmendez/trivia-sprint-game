d52e6b0f8fdf498d36e953a61ed7a792cc267fe3
Andrew Mendez <andrewjeffmendez30@gmail.com>
2024-12-01 15:18:01 +0800

    chore: rename project

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="fixed w-full py-4 text-center shadow-[1.95px_1.95px_2.6px_rgba(0,0,0,0.15)]">
-      <h1 className="mb-2 text-4xl font-bold">Quiz Game</h1>
+      <h1 className="mb-2 text-4xl font-bold">Trivia Sprint</h1>
       <p className="text-sm tracking-widest">
         &copy; Andrew Mendez {new Date().getFullYear()}
       </p>
